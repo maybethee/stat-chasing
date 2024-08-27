594dafee267388e576def16ad1c1f087c54b863c
retra <jozalamun@gmail.com>
2024-08-27 10:27:13 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>stat chasing</h1>
+      <div>
+        <h2>here are the stats:</h2>
+        <div className="statsContainer"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
